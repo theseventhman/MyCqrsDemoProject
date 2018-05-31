01ded28974465c28f9ee28bc01a6eb8fdd9f1fa1
tcn741 <tianchenlko@126.com>
2018-05-31 21:37:49 +0800

    修改

--- MyCqrsDemoProject/MyCqrsDemo.Domain.Core/Events/Event.cs
+++ MyCqrsDemoProject/MyCqrsDemo.Domain.Core/Events/Event.cs
@@ -5,8 +5,6 @@ namespace MyCqrsDemo.Domain.Core.Events
     public abstract  class Event : Message
     {
        public DateTime Timestamp { get; private set; }
-
-
         protected Event()
         {
             Timestamp = DateTime.Now;
