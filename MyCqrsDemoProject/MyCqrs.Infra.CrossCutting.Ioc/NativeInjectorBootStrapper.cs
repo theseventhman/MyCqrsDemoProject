5cfc158f1776cbec740b4adb6dabcbda98039234
tcn741 <tianchenlko@126.com>
2018-05-31 22:37:29 +0800

    添加ioc项目

---
+++ MyCqrsDemoProject/MyCqrs.Infra.CrossCutting.Ioc/NativeInjectorBootStrapper.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace MyCqrs.Infra.CrossCutting.Ioc
+{
+    public class NativeInjectorBootStrapper
+    {
+        public static void RegisterServices(IServiceCollection serivces)
+        {
+            serivces.AddSingleton<IHttpContextAccessor, IHttpContextAccessor>();
+        }
+    }
+}
